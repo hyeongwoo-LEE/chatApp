742c8b6cdf340f1acb243f701ef7c2b3704a33b5
hyeongwoo-LEE <lhw970804@naver.com>
2021-10-27 19:12:42 +0900

    Initial commit

---
+++ src/main/java/com/cos/chatApp/ChatController.java
@@ -0,0 +1,33 @@
+package com.cos.chatApp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    @CrossOrigin
+    //produces - content 타입 지정 - sse 프로토콜
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver){
+        return chatRepository.mFindBySender(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic()); //
+    }
+
+    @CrossOrigin
+    //Mono - 데이터 한번만 리턴, flux - 계속 리턴
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat){
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat); //Object 를 리턴하면 자동으로 JSON 으로 변환 (MessageConverter)
+    }
+}
